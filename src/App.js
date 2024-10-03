6e8d469e7e7a4be6792fddb2c5d7774f561de944
Prosper <piinoya@gmail.com>
2024-10-03 20:35:51 -0300

    redirect to new site

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Landing, startServer } from './pages/Landing';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Landing, startServer } from "./pages/Landing";
 
 const router = createBrowserRouter([
   { path: "/", element: <Landing />, loader: startServer },
-])
+]);
 
 function App() {
+  window.location.replace("https://pnoya.com");
   return (
     <div className="App">
       <RouterProvider router={router} />
